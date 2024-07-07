304a0ff94402c13708eaade1371e1fcc9e5b0c8f
DanilUltrakill <yaremakadanil@gmail.com>
2024-07-08 03:51:37 +0700

    Компонент для отображения контента. Также сделал FAB, TopAppBar с кнопкой Назад

---
+++ app/src/main/java/com/example/cinema_shift_2024/details/ui/ContentComponent.kt
@@ -0,0 +1,134 @@
+@file:OptIn(ExperimentalGlideComposeApi::class)
+
+package com.example.cinema_shift_2024.details.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.cinema_shift_2024.R
+import com.example.cinema_shift_2024.details.domain.entity.Details
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ContentComponent(
+    details: Details,
+    onBackArrowPressed: () -> Unit
+) {
+    TopAppBar(title = {
+        Row(
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.arrow_back),
+                contentDescription = null,
+                modifier = Modifier
+                    .clickable { onBackArrowPressed() }
+                    .padding(end = 8.dp)
+                    .background(color = Color.White),
+            )
+            Text(
+                text = details.name,
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.Center,
+            )
+        }
+    })
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        FilmInfo(details = details)
+
+        FloatingActionButton(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+        ) {
+            Text(
+                text = stringResource(R.string.show_schedule),
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+@Composable
+fun FilmInfo(details: Details) {
+    Column(
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .padding(start = 8.dp, end = 8.dp)
+    ) {
+        GlideImage(
+            model = details.img,
+            contentDescription = null,
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth()
+        )
+        TextSample(text = details.description)
+        TextSample(
+            text = stringResource(
+                R.string.genres,
+                textForList(list = details.genres)
+            )
+        )
+        TextSample(text = stringResource(R.string.realise_date, details.releaseDate))
+        TextSample(
+            text = stringResource(
+                R.string.actors,
+                textForList(list = details.actors)
+            )
+        )
+        TextSample(
+            text = stringResource(
+                R.string.directors,
+                textForList(list = details.directors)
+            )
+        )
+        TextSample(
+            text = stringResource(
+                R.string.country,
+                details.country ?: stringResource(R.string.unknown_country)
+            )
+        )
+    }
+}
+
+@Composable
+fun TextSample(text: String) {
+    Text(
+        text = text,
+        modifier = Modifier
+            .padding(top = 8.dp)
+    )
+}
+
+private fun textForList(list: List<String>): String =
+    list.joinToString(separator = ", ")
